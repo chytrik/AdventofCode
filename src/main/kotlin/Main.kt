e8a10fe75cdfbbf6b4428d61e0a506232f8d0c9a
pplch <chytrik@gmail.com>
2020-12-01 20:30:08 +0100

    upravy

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,52 +10,23 @@ fun getGreeting(): String {
     return words.joinToString(separator = " ")
 }
 
-fun readReport() {
-
-    /*
-    priklad nacteni kazdeho radku ze souboru a jeho vytisteni na terminal
-
-    File("report1.txt").forEachLine {
-        println(it)
-    }*/
-
-    /*
-    použít9 Array konstruktor, který přebírá velikost pole a funkci, která může vrátit počáteční hodnotu
-    každého prvku pole vzhledem k jeho indexu a nasledne vytisteni na terminal
-
-    val asc = Array(5) { i -> (i * i).toString() }
-    asc.forEach { println(it) }*/
-
-    /*
-    priklad vytvoreni souboru a zapisu do souboru
-    File("data.txt").writeText("Hello world!")*/
-
-    /*
-    Jednoduchá smyčka
-
-    // For loop
-    for (i in 0..10) { println (i)}
-    // ForEach
-    (0..10) .forEach { println (it)}
-    Sbírka
-    // For loop
-    val list = listOf (1, 2, 3) .filter (it == 2)
-    for (i in list) { println (i)}
-    // ForEach
-    listOf (1, 2, 3) .filter (it == 2) .forEach { println (it)}
-     */
+fun readReport1(): Pair<Int,Int> {
 
     val reportLines = File("report1.txt").readLines()
     val copyreportLines = reportLines.toList()
     reportLines.forEach{ r ->
         copyreportLines.forEach{ c ->
-            //if (c.toInt() + r.toInt() == 2020)
-            println(c)
+            if (c.toInt() + r.toInt() == 2020)  return Pair(c.toInt(), r.toInt())
         }
     }
-
+    return Pair(0,0)
 }
+
 fun main() {
     //println(getGreeting())
-    readReport()
+    // prvni zadani
+    val (x,y) = readReport1()
+    println("$x + $y, a to je " + x*y)
+    // druhe zadani
+
 }
