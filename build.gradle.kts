e3f4a08d80b142b9a6188742ca73e5c007e94d24
pplch <chytrik@gmail.com>
2020-12-01 23:19:46 +0100

    upravy, gradle, pridani druhe casti

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,15 @@
+import org.gradle.kotlin.dsl.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.20"
+    application
 }
+
+application {
+    mainClass.set("adventcode.MainKt")
+}
+
 
 group = "me.chytrik"
 version = "1.0-SNAPSHOT"
@@ -11,8 +18,24 @@ repositories {
     mavenCentral()
 }
 
-
-
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
+
+tasks.jar {
+    manifest {
+        attributes(mapOf("Implementation-Title" to project.name,
+            "Implementation-Version" to project.version,
+            "Main-Class" to "adventcode.MainKt")
+        )
+    }
+    // To add all of the dependencies otherwise a "NoClassDefFoundError" error
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
+
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,23 +10,64 @@ fun getGreeting(): String {
     return words.joinToString(separator = " ")
 }
 
-fun readReport1(): Pair<Int,Int> {
-
+fun readReport(numberVal: Int): List<Int> {
     val reportLines = File("report1.txt").readLines()
-    val copyreportLines = reportLines.toList()
-    reportLines.forEach{ r ->
-        copyreportLines.forEach{ c ->
-            if (c.toInt() + r.toInt() == 2020)  return Pair(c.toInt(), r.toInt())
+    when (numberVal) {
+        0 -> {
+            val copyreportLines = reportLines.toList()
+            reportLines.forEach { r ->
+                copyreportLines.forEach { c ->
+                    if (c.toInt() + r.toInt() == 2020) return listOf(c.toInt(), r.toInt(), 0)
+                }
+            }
+        }
+        1 -> {
+            val copyreportLines1 = reportLines.toList()
+            val copyreportLines2 = reportLines.toList()
+            reportLines.forEach { r ->
+                copyreportLines1.forEach { c1 ->
+                    copyreportLines2.forEach { c2 ->
+                        if (c1.toInt() + c2.toInt() + r.toInt() == 2020) return listOf(c1.toInt(), c2.toInt(), r.toInt())
+                    }
+                }
+            }
         }
     }
-    return Pair(0,0)
+    return listOf(0,0,0)
+}
+
+fun printMenu(): Int {
+    while (true) {
+        println("/------------------\\")
+        println("| Adventi kodovani |")
+        println("\\-----------------/")
+        println()
+        println("Menu:")
+        println("0 .. kontrola reportu, nalezeni 2 cisel davajici soucet 2020 a tisk jejich nasobku")
+        println("1 .. kontrola reportu, nalezeni 3 cisel davajici soucet 2020 a tisk jejich nasobku")
+        println()
+        println("Vyber cislo pro zvolenou operaci")
+        val inputChoice: Int = try { readLine()!!.toInt() } catch (e: NumberFormatException) { println("Chybne zadana hodnota"); 9 }
+        if (inputChoice in 0..1) return inputChoice
+        println()
+    }
 }
 
 fun main() {
-    //println(getGreeting())
-    // prvni zadani
-    val (x,y) = readReport1()
-    println("$x + $y, a to je " + x*y)
-    // druhe zadani
+    // Uvitani uz je passe, ale nechavam ho tady zakomentovane
+    // println(getGreeting())
+    val choiceMenu = printMenu()
+    when (choiceMenu) {
+        0 -> {
+            // prvni zadani
+            val (x, y) = readReport(0)
+            println("$x + $y, a to je " + x * y)
+        }
+        1 -> {
+            // druhe zadani
+            val (x, y, z) = readReport(1)
+            println("$x + $y + $z, a to je " + x * y * z)
+        }
 
+    }
 }
